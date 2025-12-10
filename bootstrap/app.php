4ebdb765060f1ade62fa1aa90ffd5a9d508e73b7
Dominikus Sebastian Ramli <sebastianramli@rocketmail.com>
2025-12-10 18:59:13 +0700

    Added trusted proxies for https connection

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(
+            trustProxies: true,
+            headers: Request::HEADER_X_FORWARDED_ALL
+        );
+        
         $middleware->alias([
             'admin' => IsAdmin::class,
         ]);
